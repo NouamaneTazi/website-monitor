d65b288acdc29829b1f27dcbeb720650a508ae5b
Nouamane Tazi <nouamane98@gmail.com>
2021-11-03 17:05:03 +0100

    fix analyze

--- internal/analyze/data.go
+++ internal/analyze/data.go
@@ -2,26 +2,28 @@ package analyze
 
 import (
 	"time"
+
+	"github.com/NouamaneTazi/iseeu/internal/inspect"
 )
 
 // UIData represents data to be passed to UI.
 type UIData struct {
-	inspectorsList    []*Inspector
+	inspectorsList    []*inspect.Inspector
 	WebsitesStatsList []WebsiteStats
 	LastTimestamp     time.Time // last updated time stamp
 }
 
 // NewUIData inits and return new ui data object.
-func NewUIData(inspectorsList []*Inspector) *UIData {
+func NewUIData(inspectorsList []*inspect.Inspector) *UIData {
 	return &UIData{inspectorsList: inspectorsList, WebsitesStatsList: make([]WebsiteStats, len(inspectorsList))}
 }
 
-// updateData collects data from inspectors and updates websites stats
-func (data *UIData) updateData(interval *time.Duration) {
+// UpdateData collects data from inspectors and updates websites stats
+func (data *UIData) UpdateData(interval time.Duration) {
 	for i := 0; i < len(data.inspectorsList); i++ {
 		inspector := data.inspectorsList[i]
-		reports := inspector.reports
-		data.WebsitesStatsList[i] = data.WebsitesStatsList[i].calculateStats(reports, interval, inspector.url)
+		reports := inspector.Reports
+		data.WebsitesStatsList[i] = data.WebsitesStatsList[i].calculateStats(reports, interval, inspector.Url)
 	}
 	// Set last updated data time
 	data.LastTimestamp = time.Now()
